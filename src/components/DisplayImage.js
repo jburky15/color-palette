2e49fc1ca3312c1dcc22757f7926e91c8df62e80
Joseph P Burkhart <jburkhart@pineenvironmental.com>
2024-01-11 10:22:10 -0500

    Colors display with hex and rgb values + copying enabled

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,35 +1,40 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from "../styles/Home.module.css"
-import ListItem from './ListItem'
+import React from "react";
+import styles from "../styles/Home.module.css";
+import ListItem from "./ListItem";
+import Image from "next/image";
 
 function DisplayImage({ uploadedImage, colorPalette }) {
   const toHex = (rgb) => {
-    let hexVal = Number(rgb).toString(16)
-    if (hexVal.length < 2) {
-      hexVal = "0" + hexVal;
+    let hex = Number(rgb).toString(16).toUpperCase();
+    if (hex.length < 2) {
+      hex = "0" + hex;
     }
-    return hexVal
-  }
+
+    return hex;
+  };
 
   return (
     <div className={styles.content}>
-        <div className='image'>
-            { uploadedImage ? <Image src={ uploadedImage } width={500} height={500} alt='uploaded' /> : <h2>Select an image in the top right</h2>}
-        </div>
+      <div className="image">
+        {uploadedImage ? (
+          <Image src={uploadedImage} width={500} height={500} alt="uploaded" />
+        ) : (
+          <h2>Select an image in the top right</h2>
+        )}
+      </div>
 
-        { 
-          colorPalette && 
-          <ul>
-            { colorPalette.map((color, idx) => {
-              const rgb = `RGB: (${color.join(", ")})`
-              const hex = `HEX: #${ toHex(color[0]) }${ toHex(color[1]) }${ toHex(color[2]) }`
-              return <h3 key={ idx }>{ rgb } { hex }</h3>
-            })}
-          </ul> 
-        }
+      {colorPalette && (
+        <ul className={styles.colors}>
+          {colorPalette.map((color, idx) => {
+            const rgb = `rgb(${color.join(",")})`;
+            const hex = `#${toHex(color[0])}${toHex(color[1])}${toHex(color[2])}`;
+
+            return <ListItem key={idx} rgb={rgb} hex={hex} />;
+          })}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default DisplayImage
+export default DisplayImage;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from "react"
 import styles from "../styles/Home.module.css"
 
-function ListItem({ rgb, hex }) {
-  return (
-    <li className=''>
+const copy = <i className="far fa-copy"></i>
 
+function ListItem({ rgb, hex }) {
+  const [hexCopied, setHexCopied] = useState(false)
+  const [rgbCopied, setRgbCopied] = useState(false)
+
+  const copyToClipboard = (e) => {
+    const color = e.target.innerText
+    navigator.clipboard.writeText(color)
+  }
+
+  return (
+    <li className={styles.colorName} style={{ background: rgb }}>
+      <span
+        className={styles.hexSpan}
+        onClick={(e) => {
+          copyToClipboard(e)
+          setHexCopied(true)
+          setTimeout(() => {
+            setHexCopied(false)
+          }, 1000)
+        }}
+      >
+        { hexCopied ? "Copied!" : hex } { copy }
+      </span>
+      <span
+        className={styles.rgbSpan}
+        onClick={(e) => {
+          copyToClipboard(e)
+          setRgbCopied(true)
+          setTimeout(() => {
+            setRgbCopied(false)
+          }, 1000)
+        }}
+      >
+        { rgbCopied ? "Copied!" : rgb } { copy }
+      </span>
     </li>
-  )
+  );
 }
 
-export default ListItem
+export default ListItem;
